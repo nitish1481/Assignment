44d39cf9cd02b1c67b0748efc9f0675d8a11ea82
nitish1481 <142612382+nitish1481@users.noreply.github.com>
2023-08-20 17:37:16 +0530

    Update Gross_Salary.py

--- Gross_Salary.py
+++ Gross_Salary.py
@@ -1,1 +1,14 @@
-
+def Gross_Salary(basic,grade):
+  hra=basic*0.2
+  da=basic*0.5
+  allow
+  if(grade=='A'):
+    allow=1700
+  elif(grade=='B'):
+    allow=1500
+  elif(grade=='C'):
+    allow=1300
+  pf=basic*0.11
+  Salary=basic+hra+da+allow-pf
+  return Salary
+  
