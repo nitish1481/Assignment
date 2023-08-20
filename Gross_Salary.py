6067038d45f667ec14bf369ff65e36f52d47b5f3
nitish1481 <142612382+nitish1481@users.noreply.github.com>
2023-08-20 20:47:23 +0530

    Update Gross_Salary.py

--- Gross_Salary.py
+++ Gross_Salary.py
@@ -1,7 +1,7 @@
 def Gross_Salary(basic,grade):
   hra=basic*0.2
   da=basic*0.5
-  allow
+  allow=0
   if(grade=='A'):
     allow=1700
   elif(grade=='B'):
@@ -11,4 +11,14 @@ def Gross_Salary(basic,grade):
   pf=basic*0.11
   Salary=basic+hra+da+allow-pf
   return Salary
-  
+
+
+
+basic=0
+grade=input("Enter grade of Employee\n")
+if(grade=='A'):
+  basic=10000
+elif(grade=='B'):
+  basic=4567
+salary=Gross_Salary(basic,grade)
+print("The Gross Salary of Employee is: ",salary)
